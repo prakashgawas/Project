916288c8ff536afb9a93a73f986251a7086787de
gunners009 <prakashgawas009@gmail.com>
2016-09-08 00:00:22 +0530

    base ready

--- LP Inventory/(s,S) policy/Inv_sS.m
+++ LP Inventory/(s,S) policy/Inv_sS.m
@@ -1,82 +1,73 @@
 
 clear all;
 N = 10;% Number of items you can order
-M = 20;% Number of states
+M = 10;% Number of states
 Time = 10;
-S = 0:1:M ; % states
+S = 0:1:M ;% states
+A= 0:1:N;%actions
 T = 0:1:Time;
-p = repmat(10/11,11,1); 
+p = 1/11; 
+
+oc=2;%purchase cost
+sc=0.4; %shortage cost
+hc=0.2; %holding cost
 %%
 
-F = zeros(1,N+1);
-for s=1:length(S)
-    i=1;
-    while (i < s)
-        F(s)= F(s)+ 8*S(i)*p(i);
-        i=i+1;
-    end
-    F(s)=F(s)+8*S(i)*sum(p(i:M));
-end
-
 %Expected reward
-r=zeros(N+1,N+1);
+r=zeros(M+1,N+1);
 for s=1:length(S)
-    i=0;
-    for a=1:length(S)
-        if(S(s)+S(a)<4)
-            if (S(a)==0)
-                r(s,a)=F(S(s)+S(a)+1)-S(s);
-      
+    for a=1:length(A)
+        TOC=oc*A(a);
+        z=0;
+        THC=0;
+        TSC=0;
+        while(z<=10)
+            if(z<=S(s)+A(a))
+                THC=THC+ hc*(S(s)+A(a)-z)*p;
             else
-                r(s,a)= F(S(s)+S(a)+1)-4-S(s)-3*S(a);
+                TSC=TSC+sc*(z-S(s)-A(a))*p;
+                
             end
-            i=i+1;
+            
+            z=z+1;
         end
+        r(s,a)=TOC +THC+TSC;
     end
 end
     
 %transition probability
-prob1=zeros(N+1,N+1);
-for s=1:length(S)
+prob1=zeros(M+1,N+1);
+
+for s=1:M+N+1
     j=s;
     z=1;
-    while(j>1&&j>s-4)    
-            prob1(s,j)=p(S(s)-S(j)+1);
+    while(j>1&&j>s-10)    
+            prob1(s,j)=p;
             z=z-prob1(s,j);
             j=j-1;
     end
     prob1(s,j)=z;
 end
             
-prob=zeros(N+1,N+1,N+1);
+prob=zeros(M+1,M+1,N+1);
 
 for s=1:length(S)
     for a=1:length(S)
-        if(S(s)+S(a)<=M)
+        if(S(s)+A(a)<=M)
            for j=1:length(S)
                 if(j<=s+a)
-                    prob(s,j,a)=prob1(S(s)+S(a)+1,S(j)+1);
+                    prob(s,j,a)=prob1(S(s)+A(a)+1,S(j)+1);
                 end
            end
         end
     end
 end
 
-for s=1:length(S)
-    a=length(S)-s+1;
-    while(a>=1)
-        q(s,a)=1;
-        a=a-1;
-    end
-end
-
-
-        
 
                  %% data file for Solver %%%%%%%%%
 disp('File writing');
 
-fileID = fopen('Inv_FH.dat','w');
+fileID = fopen('Inv_sS.dat','w');
 
 % M Value
 fprintf(fileID,'param M := %d;\n', M);
@@ -84,39 +75,19 @@ fprintf(fileID,'param M := %d;\n', M);
 % N Value
 fprintf(fileID,'param N := %d;\n', N);
 
+% Time Value
+fprintf(fileID,'param Time := %d;\n', Time);
+
 %lambda
-fprintf(fileID,'param lambda:= 0.95;\n');
+%fprintf(fileID,'param lambda:= 0.95;\n');
 
 
 % for set states
-fprintf(fileID,'set S := ');
-for i1 = 0:N
+fprintf(fileID,'set S := \t');
+for i1 = 0:M
     fprintf(fileID,'%d \t',i1);
 end
 fprintf(fileID,';\n');
-
-
-% for time epochs
-fprintf(fileID,'set T := ');
-for i1 = 0:Time
-    fprintf(fileID,'%d \t',i1);
-end
-fprintf(fileID,';\n');
-
-
-% for alpha
-fprintf(fileID,'param alpha := ');
-for i1 = 0:N
-    fprintf(fileID,'%d 0.25\n',i1);
-end
-fprintf(fileID,';\n');
-
-% for set time_epoch
-%fprintf(fileID,'set time_epoch := \t');
-%for i2 = 0:T-1
-%    fprintf(fileID,'%d \t',i2);
-%end
-%fprintf(fileID,';\n');
 
 % for set actions
 fprintf(fileID,'set A := \t');
@@ -126,21 +97,27 @@ end
 fprintf(fileID,';');
 fprintf(fileID,'\n');
 
-
-%q
-fprintf(fileID,'param q :');
-for s1 = 1:length(S)
-    fprintf(fileID,' %d \t', s1-1);
-end
-fprintf(fileID,':= \n');
-for s = 1:length(S) 
-    fprintf(fileID,' %d \t', s-1);
-    for s1 = 1:length(S)
-        fprintf(fileID,'%1.15f \t', q(s,s1));
-    end
-    fprintf(fileID,' \n');
+% for time epochs
+fprintf(fileID,'set T := \t');
+for i1 = 0:Time
+    fprintf(fileID,'%d \t',i1);
 end
 fprintf(fileID,';\n');
+
+
+% for alpha
+fprintf(fileID,'param alpha := ');
+for i1 = 0:N
+    fprintf(fileID,'%d 0.0909090\n',i1);
+end
+fprintf(fileID,';\n');
+
+% for set time_epoch
+%fprintf(fileID,'set time_epoch := \t');
+%for i2 = 0:T-1
+%    fprintf(fileID,'%d \t',i2);
+%end
+%fprintf(fileID,';\n');
 
 % for rewards
 
@@ -173,8 +150,9 @@ for a = 1:length(S)
     end
     fprintf(fileID,' \n');
 end
+        
 
-
+ 
 
 
 
